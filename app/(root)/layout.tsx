99c715432c8216944865f4f5fcb0b1c5cb712fe8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-14 05:45:00 +0600

    decorate some imported common components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,12 @@
-import Bottombar from "@/components/shared/Bottombar";
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
-import Topbar from "@/components/shared/Topbar";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
