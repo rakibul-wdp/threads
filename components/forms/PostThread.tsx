678be5c2a7c66bb78ca72c09d603f4723aa2b7d6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-23 05:55:00 +0600

    create thread and comment validation

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,4 +1,50 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { updateUser } from "@/lib/actions/user.actions";
+import { useUploadThing } from "@/lib/uploadthing";
+import { isBase64Image } from "@/lib/utils";
+import { UserValidation } from "@/lib/validations/user";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
 const PostThread = ({ userId }: { userId: string }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
+
   return <div>PostThread</div>;
 };
 
