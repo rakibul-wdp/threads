5a2e771800eb9c515ed8c8e554d643a9633b94a0
rakibul-wdp <rakibul.wdp@gmail.com>
2023-04-26 05:05:00 +0600

    create threads tab component boilerplate and recive props

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+}
+
+const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
+  return <div>ThreadsTab</div>;
+};
+
+export default ThreadsTab;
