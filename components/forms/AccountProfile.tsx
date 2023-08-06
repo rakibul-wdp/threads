c3651f80919817aafb284c5c37a1204ec3bc2bb1
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-06 06:46:05 +0600

    install textarea ui component from shadcn

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -9,6 +9,7 @@ import {
   FormLabel,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { UserValidation } from "@/lib/validations/user";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Image from "next/image";
