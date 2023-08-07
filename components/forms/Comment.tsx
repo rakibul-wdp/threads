15fe68dffe5938cb6a8ecd3167a0ed8a0ea085e4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-07 10:39:21 +0600

    post thread code decorate and create comment form

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,8 +1,81 @@
-const Comment = () => {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { CommentValidation } from "@/lib/validations/thread";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Input } from "../ui/input";
+
+interface Props {
+  threadId: string;
+  currentUserImg: string;
+  currentUserId: string;
+}
+
+const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(CommentValidation),
+    defaultValues: {
+      thread: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof CommentValidation>) => {
+    // await createThread({
+    //   text: values.thread,
+    //   author: userId,
+    //   communityId: null,
+    //   path: pathname,
+    // });
+
+    router.push("/");
+  };
+
   return (
-    <div>
-      <h1 className="text-white">Comment form</h1>
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-10 flex flex-col justify-start gap-10"
+      >
+        <FormField
+          control={form.control}
+          name="thread"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full gap-3">
+              <FormLabel className="text-base-semibold text-light-2">
+                Content
+              </FormLabel>
+              <FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
+                <Input
+                  type="text"
+                  placeholder="Comment..."
+                  className="no-focus text-light-1 outline-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="bg-primary-500">
+          Post Thread
+        </Button>
+      </form>
+    </Form>
   );
 };
 
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -9,32 +9,13 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { createThread } from "@/lib/actions/thread.actions";
-import { updateUser } from "@/lib/actions/user.actions";
-import { useUploadThing } from "@/lib/uploadthing";
-import { isBase64Image } from "@/lib/utils";
 import { ThreadValidation } from "@/lib/validations/thread";
-import { UserValidation } from "@/lib/validations/user";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { ChangeEvent, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-
-interface Props {
-  user: {
-    id: string;
-    objectId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-  };
-  btnTitle: string;
-}
 
 const PostThread = ({ userId }: { userId: string }) => {
   const router = useRouter();
