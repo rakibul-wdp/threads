4c0aae49dd4a8709821510448fb0dfbd10cabcf5
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-07 10:25:52 +0600

    create comment component boilerplate

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,4 +1,6 @@
 import ThreadCard from "@/components/cards/ThreadCard";
+import Comment from "@/components/forms/Comment";
+import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -12,20 +14,26 @@ const Page = async ({ params }: { params: { id: string } }) => {
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
+  const thread = await fetchThreadById(params.id);
+
   return (
     <section className="relative">
       <div>
         <ThreadCard
-          key={post._id}
-          id={post._id}
+          key={thread._id}
+          id={thread._id}
           currentUserId={user?.id || ""}
-          parentId={post.parentId}
-          content={post.text}
-          author={post.author}
-          community={post.community}
-          createdAt={post.createdAt}
-          comments={post.children}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={thread.author}
+          community={thread.community}
+          createdAt={thread.createdAt}
+          comments={thread.children}
         />
+      </div>
+
+      <div className="mt-7">
+        <Comment />
       </div>
     </section>
   );
---
+++ components/forms/Comment.tsx
@@ -0,0 +1,9 @@
+const Comment = () => {
+  return (
+    <div>
+      <h1 className="text-white">Comment form</h1>
+    </div>
+  );
+};
+
+export default Comment;
