66c27ae23b6d1a45764cd8fd2cf4cc2353344a95
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-08 10:19:13 +0600

    show avatar name and username by user card

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface Props {
   id: string;
   name: string;
@@ -7,7 +9,24 @@ interface Props {
 }
 
 const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
-  return <div>UserCard</div>;
+  return (
+    <article className="user-card">
+      <div className="user-card_avatar">
+        <Image
+          src={imgUrl}
+          alt="logo"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+
+        <div className="flex-1 text-ellipsis">
+          <h4 className="text-base-semibold text-light-1">{name}</h4>
+          <p className="text-small-medium text-gray-1">@{username}</p>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default UserCard;
