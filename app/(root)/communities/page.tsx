5a45740ad7c60ba7fd024a2cef2218d28f23532d
rakibul-wdp <rakibul.wdp@gmail.com>
2023-08-08 09:50:17 +0600

    create fetch users function in server side and receive param and also declare type

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,4 +1,21 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { profileTabs } from "@/constants";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
 const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // Fetch users
+
   return (
     <section>
       <h1 className="head-text mb-10">Communities</h1>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import { SortOrder } from "mongoose";
 
 interface Params {
   userId: string;
@@ -84,4 +85,26 @@ export async function fetchUserPosts(userId: string) {
   } catch (error: any) {
     throw new Error(`Failed to fetch user posts: ${error.message}`);
   }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, "i");
+  } catch (error) {}
 }
